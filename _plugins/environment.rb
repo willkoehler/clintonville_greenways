6e241d071c1b2922c823d7f4d82b0cd5953ef3e7
Will Koehler <wck555@gmail.com>
2022-02-20 12:22:15 -0500

    Run standardrb --fix

--- _plugins/environment.rb
+++ _plugins/environment.rb
@@ -2,17 +2,15 @@
 # See https://gist.github.com/nicolashery/5756478
 
 module Jekyll
-
   class EnvironmentVariablesGenerator < Generator
-
     def generate(site)
-      site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
+      site.config["env"] = ENV["JEKYLL_ENV"] || "development"
       # Add other environment variables to `site.config` here...
 
       # Set config values based on environment
-      case site.config['env']
-      when 'development'
-        site.config['assets']['debug'] = true
+      case site.config["env"]
+      when "development"
+        site.config["assets"]["debug"] = true
       end
     end
   end
